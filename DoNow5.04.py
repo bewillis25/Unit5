77135c146b03e15a973d68d5e5f563f5d04948b9
bewillis25 <bewillis25@students.bmchs.org>
2022-05-13 09:01:18 -0500

    Do Now 5.04

---
+++ DoNow5.04.py
@@ -0,0 +1,46 @@
+'''
+###############
+# Do Now 5.04 #
+###############
+
+1. In your Console
+------------------
+Type the following:
+my_dictionary = {
+'a': 1,
+'b': 2,
+'c': 3,
+'d': 4,
+'e': 5
+}
+print(list(my_dictionary))
+
+In your Notebook
+----------------
+Respond to the following:
+Write down what list() does when given a dictionary. It turns the dictionary keys into a list
+
+What type does list() return? list
+
+Write down how you might use a for loop to go through and print the values of my_dictionary. 
+
+2. In your Console
+------------------
+Challenge yourself
+Try your solution to 3.
+'''
+my_dictionary = {
+'a': 1,
+'b': 2,
+'c': 3,
+'d': 4,
+'e': 5
+}
+print(list(my_dictionary.values()))
+
+
+
+
+
+
+
